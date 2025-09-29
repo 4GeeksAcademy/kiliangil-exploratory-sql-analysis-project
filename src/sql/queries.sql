6b5f8e50bf5d5e49832ff849a0314ea517a9b71a
Kiliangil88 <149784768+Kiliangil88@users.noreply.github.com>
2025-09-29 19:25:35 +0000

    primeros 8 ejercicios completados

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -1,20 +1,3 @@
--- PLEASE READ THIS BEFORE RUNNING THE EXERCISE
-
--- ⚠️ IMPORTANT: This SQL file may crash due to two common issues: comments and missing semicolons.
-
--- ✅ Suggestions:
--- 1) Always end each SQL query with a semicolon `;`
--- 2) Ensure comments are well-formed:
---    - Use `--` for single-line comments only
---    - Avoid inline comments after queries
---    - Do not use `/* */` multi-line comments, as they may break execution
-
--- -----------------------------------------------
--- queries.sql
--- Complete each mission by writing your SQL query
--- directly below the corresponding instruction
--- -----------------------------------------------
-
 SELECT * FROM regions;
 SELECT * FROM species;
 SELECT * FROM climate;
@@ -23,30 +6,42 @@ SELECT * FROM observations;
 
 -- MISSION 1
 -- Your query here;
+select * FROM climate LIMIT 10;
 
 -- MISSION 2
 -- Your query here;
-
+select DISTINCT "region_id" FROM regions;
 
 -- MISSION 3
 -- Your query here;
-
+select DISTINCT COUNT("species_id") FROM species;
 
 -- MISSION 4
 -- Your query here;
 
+select COUNT("region_id") FROM observations WHERE "region_id" = 2;
 
 -- MISSION 5
 -- Your query here;
-
-
+SELECT * FROM observations
+WHERE observation_date = '1998-08-08';
 -- MISSION 6
 -- Your query here;
-
+SELECT COUNT(region_id)
+FROM observations
+GROUP BY region_id;
 
 -- MISSION 7
 -- Your query here;
-
+select species_id, COUNT(*) AS total_records
+FROM observations
+GROUP BY species_id
+ORDER BY total_records DESC
+LIMIT 5;
 
 -- MISSION 8
 -- Your query here;
+SELECT * 
+FROM observations
+GROUP BY  species_id
+HAVING COUNT(species_id)<5;
